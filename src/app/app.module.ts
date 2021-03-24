074fb273d03880980d915c2f92b51317fcd42904
superpuper777 <superfevrik@gmail.com>
2021-03-24 23:34:53 +0300

    feat: add material and add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { MainModule } from './main/main.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     MaterialModule,
     MainModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatTooltipModule],
+  exports: [CommonModule, MatIconModule, MatTooltipModule],
 })
 export class MaterialModule {}
